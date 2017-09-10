8633063236f616292da88b66406443ed6bc234e1
Ellest <syunelli@gmail.com>
2017-09-10 14:13:48 -0500

    update

--- sommelier/settings.py
+++ sommelier/settings.py
@@ -142,6 +142,7 @@ SITE_ID = 1
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 """
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
+
